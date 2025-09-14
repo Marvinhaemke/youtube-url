b8c2bb344593b34c2518896e0fbb39cda9bc61e5
Marvinhaemke <66739755+Marvinhaemke@users.noreply.github.com>
2025-09-14 13:31:34 +0200

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,10 @@
+// Simple A/B Testing Middleware for Vercel Edge Functions
+// This version works with static HTML files without Next.js
+
+export default async function middleware(request) {
+    return;
+}
+
+export const config = {
+  matcher: '/'
+};
